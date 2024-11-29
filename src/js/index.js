423c02f299f0c3ea60e93cb5a04c9ae7f7c85ecf
jungminji <wjdalswl0215@gmail.com>
2024-11-30 04:32:17 +0900

    refac : 이벤트 바인딩 함수랑 재사용하는 함수 위치 분리

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,24 @@
  * - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
  */
 
+/**
+ * [메뉴 수정]
+ * 메뉴 수정 버튼 클릭 이벤트를 받고, prompt 가 뜬다.
+ * prompt 창에서 신규 메뉴명을 입력 받는다.
+ * 확인 버튼을 누르면 입력 받은 메뉴가 수정된다.
+ */
+
+/**
+ * [메뉴 삭제]
+ * 메뉴 삭제 버튼을 누르면 confirm 창이 뜬다.
+ * confirm 에서 확인 버튼을 누르면 메뉴가 삭제된다.
+ */
+
+/**
+ * 리팩토링 TIP
+ * 이벤트 바인딩 함수랑 재사용하는 함수를 일관성있게 위치해두자.
+ */
+
 const $ = (selector) => document.querySelector(selector);
 
 // 이 함수가 실행해야함
@@ -20,35 +38,8 @@ function App() {
     $(".menu-count").innerText = `총 ${menuCount} 개`;
   };
 
-  $("#espresso-menu-list").addEventListener("click", (e) => {
-    /** 메뉴 수정 */
-    // 수정 버튼에 이벤트를 주려고했는데, 코드를 짜는 시점에 수정 버튼이 없다.
-    // 이럴때 "이벤트 위임"이라는 것을 통해 해결할 수 있다.
-    // 즉, 요소가 아직 없을 때, 다른애한테 이벤트를 먼저 받고있으라고 위임할 수 있다.
-    if (e.target.classList.contains("menu-edit-button")) {
-      const $menuName = e.target.closest("li").querySelector(".menu-name");
-      let updatedMenuName = prompt(
-        "메뉴명을 수정해주세요.",
-        $menuName.innerText
-      );
-      $menuName.innerText = updatedMenuName;
-    }
-
-    /** 메뉴 삭제 */
-    if (e.target.classList.contains("menu-remove-button")) {
-      if (confirm("정말 삭제하시겠습니까?")) {
-        e.target.closest("li").remove();
-        updateMenuCount();
-      }
-    }
-  });
-
-  // form 태그가 자동으로 전송되는걸 막아준다.
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-  });
-
-  const addMenu = () => {
+  // 재사용하는 함수끼리
+  const addMenuName = () => {
     if ($("#espresso-menu-name").value === "") {
       alert("메뉴를 입력하세요!");
       return;
@@ -58,20 +49,20 @@ function App() {
 
     const menuItemTemplate = (espressoMenuName) => {
       return `<li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-            <button
-              type="button"
-              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
-              수정
-            </button>
-            <button
-              type="button"
-              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-            >
-              삭제
-            </button>
-            </li>`;
+              <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+              <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+              >
+                수정
+              </button>
+              <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+              >
+                삭제
+              </button>
+              </li>`;
     };
 
     $("#espresso-menu-list").insertAdjacentHTML(
@@ -89,30 +80,48 @@ function App() {
     $("#espresso-menu-name").value = "";
   };
 
-  $("#espresso-menu-submit-button").addEventListener("click", () => {
-    addMenu();
+  const updateMenuName = (e) => {
+    const $menuName = e.target.closest("li").querySelector(".menu-name");
+    let updatedMenuName = prompt("메뉴명을 수정해주세요.", $menuName.innerText);
+    $menuName.innerText = updatedMenuName;
+  };
+
+  const removeMenuName = (e) => {
+    if (confirm("정말 삭제하시겠습니까?")) {
+      e.target.closest("li").remove();
+      updateMenuCount();
+    }
+  };
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    /** 메뉴 수정 */
+    // 수정 버튼에 이벤트를 주려고했는데, 코드를 짜는 시점에 수정 버튼이 없다.
+    // 이럴때 "이벤트 위임"이라는 것을 통해 해결할 수 있다.
+    // 즉, 요소가 아직 없을 때, 다른애한테 이벤트를 먼저 받고있으라고 위임할 수 있다.
+    if (e.target.classList.contains("menu-edit-button")) {
+      updateMenuName(e);
+    }
+
+    /** 메뉴 삭제 */
+    if (e.target.classList.contains("menu-remove-button")) {
+      removeMenuName(e);
+    }
   });
+
+  // form 태그가 자동으로 전송되는걸 막아준다.
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
 
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
       return;
     }
 
-    addMenu();
+    addMenuName();
   });
 }
 
 App();
-
-/**
- * [메뉴 수정]
- * 메뉴 수정 버튼 클릭 이벤트를 받고, prompt 가 뜬다.
- * prompt 창에서 신규 메뉴명을 입력 받는다.
- * 확인 버튼을 누르면 입력 받은 메뉴가 수정된다.
- */
-
-/**
- * [메뉴 삭제]
- * 메뉴 삭제 버튼을 누르면 confirm 창이 뜬다.
- * confirm 에서 확인 버튼을 누르면 메뉴가 삭제된다.
- */
