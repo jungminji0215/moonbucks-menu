411be102cd3f6d0b38f384d5f1d1758856fde8af
jungminji <wjdalswl0215@gmail.com>
2024-11-29 16:10:19 +0900

    feat : form 태그 새로고침 막기

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,34 @@
 
 /**
  * [메뉴 추가]
- * 메뉴 이름을 입력 받는다.
- * 입력한 메뉴를 엔터키 입력으로 추가한다.
- * 총 메뉴 갯수를 count 하여 상단에 보여준다.
- * 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
- * 메뉴가 추가되고 나면, input 은 빈 값으로 초기화한다.
- * 사용자 입력값이 빈 값이라면 추가되지 않는다.
+ * - [ ] 메뉴 이름을 입력 받는다.
+ * - [ ] 입력한 메뉴를 엔터키 입력으로 추가한다.
+ * - [ ] 총 메뉴 갯수를 count 하여 상단에 보여준다.
+ * - [ ] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+ * - [ ] 메뉴가 추가되고 나면, input 은 빈 값으로 초기화한다.
+ * - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
  */
+
+// 이 함수가 실행해야함
+function App() {
+  // form 태그가 자동으로 전송되는걸 막아준다.
+  document
+    .querySelector("#espresso-menu-form")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+  const $espressoMenuName = document
+    .querySelector("#espresso-menu-name")
+    .addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        const menuInput = document.querySelector("#espresso-menu-name").value;
+        console.log("menuInput :>> ", menuInput);
+      }
+    });
+}
+
+App();
 
 /**
  * [메뉴 수정]
